bcb54066df509a1a6ece0050c0485fa273f3f8e7
stack <bradfunk@gmail.com>
2024-04-11 19:58:12 -0700

    ff

--- src/capabilities/projected-income/hooks/use-has-merit-pairs.ts
+++ src/capabilities/projected-income/hooks/use-has-merit-pairs.ts
@@ -7,8 +7,13 @@ import { findSameYear } from "shared/utility/find-same-year";
 export const useHasMeritPairs = () => {
   const timeSeries = useStore(store, (x) => x.projectedIncome.timeSeries);
   return useMemo(() => {
-    return timeSeries.meritIncreasePct.every((x) => {
-      return !!findSameYear(DateTime.fromISO(x.date).year, timeSeries.meritBonusPct);
-    });
+    return (
+      timeSeries.meritIncreasePct.every((x) => {
+        return !!findSameYear(DateTime.fromISO(x.date).year, timeSeries.meritBonusPct);
+      }) &&
+      timeSeries.meritBonusPct.every((x) => {
+        return !!findSameYear(DateTime.fromISO(x.date).year, timeSeries.meritIncreasePct);
+      })
+    );
   }, [timeSeries.meritBonusPct, timeSeries.meritIncreasePct]);
 };
