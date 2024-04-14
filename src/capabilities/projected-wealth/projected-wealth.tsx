c32ec7d2d691a89e52b7baf7ecc85c3ff5317f34
stack <bradfunk@gmail.com>
2024-04-14 11:36:34 -0700

    cleanup

--- src/capabilities/projected-wealth/projected-wealth.tsx
+++ src/capabilities/projected-wealth/projected-wealth.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { FutureEvents } from "./components/future-events";
 import { WealthChart } from "./components/wealth-chart";
 import { WealthTable } from "./components/wealth-table";
@@ -6,10 +6,8 @@ import { WealthTable } from "./components/wealth-table";
 export const ProjectedWealth = () => {
   return (
     <Box display="flex" flexDirection="row" height="100%" width={"100%"} gap={2}>
-      <Box flex="0 1 auto">
-        <Paper sx={{ flex: "0 1 auto", padding: 2, height: "100%", overflow: "auto" }}>
-          <FutureEvents />
-        </Paper>
+      <Box flex="0 1 auto" height={"100%"} overflow={"auto"}>
+        <FutureEvents />
       </Box>
       <Box flex="1 1 auto" overflow={"auto"}>
         <Stack spacing={2} height={"100%"} width={"100%"}>
