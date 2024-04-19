d648bd9216fbad5bfbaed102d7ae4ecda1586e96
stack <bradfunk@gmail.com>
2024-04-19 16:31:44 -0700

    ff

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,26 +9,24 @@ export const App = () => {
   const [tab, setTab] = useState<string>("wealth");
 
   return (
-    <>
-      <Box display={"flex"} flexDirection={"column"} height="100%">
-        <Box flex="0 1 auto">
-          <Stack direction="row">
-            <Tabs value={tab} onChange={(_, value) => setTab(value)}>
-              <Tab value="wealth" label="Total Wealth" />
-              <Tab value="projected-income" label="Projected Income" />
-              <Tab value="projected-wealth" label="Projected Wealth" />
-            </Tabs>
-            <Box marginLeft={"auto"} gap={2} display={"flex"}>
-              <ConfigModal />
-            </Box>
-          </Stack>
-        </Box>
-        <Box flex="1 1 auto" padding={2} height={"100%"} width={"100%"} overflow="hidden">
-          {tab === "wealth" && <NetWealth />}
-          {tab === "projected-income" && <ProjectedIncome />}
-          {tab === "projected-wealth" && <ProjectedWealth />}
-        </Box>
+    <Box padding={2} paddingTop={0} display={"flex"} flexDirection={"column"} height="100%" gap={2}>
+      <Box flex="0 1 auto">
+        <Stack direction="row">
+          <Tabs value={tab} onChange={(_, value) => setTab(value)}>
+            <Tab value="wealth" label="Total Wealth" />
+            <Tab value="projected-income" label="Projected Income" />
+            <Tab value="projected-wealth" label="Projected Wealth" />
+          </Tabs>
+          <Box marginLeft={"auto"} gap={2} display={"flex"}>
+            <ConfigModal />
+          </Box>
+        </Stack>
       </Box>
-    </>
+      <Box flex="1 1 auto" height={"100%"} width={"100%"} overflow="hidden">
+        {tab === "wealth" && <NetWealth />}
+        {tab === "projected-income" && <ProjectedIncome />}
+        {tab === "projected-wealth" && <ProjectedWealth />}
+      </Box>
+    </Box>
   );
 };
