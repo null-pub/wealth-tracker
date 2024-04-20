1b4987175642d70562f385befba9afa19ad436bd
stack <bradfunk@gmail.com>
2024-04-20 13:02:37 -0700

    ff

--- src/shared/utility/format-cash.ts
+++ src/shared/utility/format-cash.ts
@@ -2,7 +2,7 @@ export const formatCashShort = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   notation: "compact",
-  maximumSignificantDigits: 3,
+  maximumFractionDigits: 0,
 }).format;
 
 export const formatCash = new Intl.NumberFormat("en-US", {
