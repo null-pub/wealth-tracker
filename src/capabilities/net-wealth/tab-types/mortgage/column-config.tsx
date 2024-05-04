c6a65779a271829ea0eaa5333740cd77dc6d14e4
stack <bradfunk@gmail.com>
2024-05-04 10:13:26 -0700

    ff

--- src/capabilities/net-wealth/tab-types/mortgage/column-config.tsx
+++ src/capabilities/net-wealth/tab-types/mortgage/column-config.tsx
@@ -5,21 +5,36 @@ import { CustomCellRendererProps } from "ag-grid-react";
 import { DateTime } from "luxon";
 import { Cash } from "shared/components/formatters/cash";
 import { AccountData } from "shared/models/store/current";
-import { removeAccountEntry } from "shared/store";
+import { removeAccountEntry, updateAccountDate, updateAccountValue } from "shared/store";
 import { shortDate } from "shared/utility/format-date";
 
 export const createAccountColumnConfig = (accountName: string): ColDef<AccountData>[] => [
   {
+    cellEditor: "agDateCellEditor",
+    editable: true,
     headerName: "Date",
     sort: "desc",
     valueFormatter: (x) => x.value?.toFormat(shortDate),
     valueGetter: (x) => x.data && DateTime.fromISO(x.data.date),
+    valueSetter: (x) => {
+      const date = DateTime.fromJSDate(x.newValue);
+      if (date.isValid) {
+        updateAccountDate(accountName, x.data, date);
+      }
+      return date.isValid;
+    },
   },
   {
+    editable: true,
+    cellEditor: "agNumberCellEditor",
     headerName: "Home Value",
     cellRenderer: (x: CustomCellRendererProps<AccountData>) => <Cash value={x.value} placement="left" />,
     valueGetter: (x) => x.data?.value,
     type: "numericColumn",
+    valueSetter: (x) => {
+      updateAccountValue(accountName, x.data, +x.newValue);
+      return true;
+    },
   },
   {
     cellStyle: () => ({ display: "inline-flex", padding: "0px" }),
