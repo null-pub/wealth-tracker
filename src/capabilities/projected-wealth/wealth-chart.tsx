d3a04417727628d52bb6181683e702a2516a2d8e
stack <bradfunk@gmail.com>
2024-08-15 23:18:12 -0700

    fix

--- src/capabilities/projected-wealth/wealth-chart.tsx
+++ src/capabilities/projected-wealth/wealth-chart.tsx
@@ -5,7 +5,21 @@ import { useMemo } from "react";
 import { getLocalDateTime } from "shared/utility/current-date";
 import { formatCash, formatCashShort } from "shared/utility/format-cash";
 import { shortDate } from "shared/utility/format-date";
-import { useTimeSeriesWealth } from "./hooks/use-times-series-wealth";
+import { TimeSeriesWealth, useTimeSeriesWealth } from "./hooks/use-times-series-wealth";
+
+interface MarkerParams<T> {
+  fill: string;
+  fillOpacity: number;
+  highlighted: boolean;
+  seriesId: string;
+  size: number;
+  stroke: string;
+  strokeOpacity: number;
+  strokeWidth: number;
+  xKey: string;
+  yKey: string;
+  datum: T;
+}
 
 export const WealthChart = (props: { titleYear: number }) => {
   const dataYear = getLocalDateTime().year + 1;
@@ -25,16 +39,53 @@ export const WealthChart = (props: { titleYear: number }) => {
             content: `${DateTime.fromJSDate(datum[xKey]).toISODate()} ${formatCash(datum[yKey])}`,
           }),
         },
+        marker: {
+          formatter: (params: MarkerParams<TimeSeriesWealth>) => {
+            if (params.datum.date.year === DateTime.local().plus({ years: 1 }).year) {
+              return {
+                fill: "orange",
+              };
+            } else if (params.datum.date.year === DateTime.local().plus({ years: 2 }).year) {
+              return {
+                fill: "rgb(244, 67, 54)",
+              };
+            } else if (params.datum.date.hasSame(DateTime.local(), "day")) {
+              return {
+                fill: "green",
+              };
+            }
+          },
+        },
       },
       {
         type: "line",
         xKey: "graphDate",
         yKey: "yoyCash",
         yName: "YoY ($)",
+        stroke: "grey",
+
         tooltip: {
           renderer: ({ datum, yKey, xKey }) => ({
             content: `${DateTime.fromJSDate(datum[xKey]).toISODate()} ${formatCash(datum[yKey])}`,
           }),
+        },
+        marker: {
+          fill: "grey",
+          formatter: (params: MarkerParams<TimeSeriesWealth>) => {
+            if (params.datum.date.year === DateTime.local().plus({ years: 1 }).year) {
+              return {
+                fill: "orange",
+              };
+            } else if (params.datum.date.year === DateTime.local().plus({ years: 2 }).year) {
+              return {
+                fill: "rgb(244, 67, 54)",
+              };
+            } else if (params.datum.date.hasSame(DateTime.local(), "day")) {
+              return {
+                fill: "green",
+              };
+            }
+          },
         },
       },
     ] as AgLineSeriesOptions[];
