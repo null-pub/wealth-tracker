a65af06ef95da648d28f3381640e1bdcc841e691
Nullberri <bradfunk@gmail.com>
2024-12-30 11:08:26 -0800

    fix past pay dates impacting threshold tax

--- src/capabilities/projected-wealth/hooks/use-future-totals.tsx
+++ src/capabilities/projected-wealth/hooks/use-future-totals.tsx
@@ -15,7 +15,7 @@ const isFuture = (date: DateTime) => date.diffNow("milliseconds").milliseconds >
 const thresholdTaxRemaining = (taxRate: number, threshold: number, scenario: Scenario) => {
   const remaining = scenario.payments
     .slice(scenario.currentPaymentIdx)
-    .filter((x) => x.cumulative >= threshold)
+    .filter((x) => x.cumulative >= threshold && isFuture(DateTime.fromISO(x.payedOn)))
     .reduce((acc, curr) => {
       return acc + Math.min(curr.value, curr.cumulative - threshold) * taxRate;
     }, 0);
